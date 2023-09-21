649fb3c33a2cf017b5973bd5b26e9e843de82559
Gowri <Gowri@192.168.0.88>
2023-09-21 20:15:08 +0530

    script to find the total number of links present on google page

--- Selenium-WebDriver/src/test/java/webelement_link/Print_Total_Number_Of_Links.java
+++ Selenium-WebDriver/src/test/java/webelement_link/Print_Total_Number_Of_Links.java
@@ -16,7 +16,6 @@ public class Print_Total_Number_Of_Links {
 		driver.get("https://www.google.co.in/");
 
 		List<WebElement> links = driver.findElements(By.tagName("a"));
-		// List<WebElement> links = driver.findElements(By.xpath("//a"));
 
 		int number_of_links = links.size();
 		System.out.println("Total number of links present on webpage: " + number_of_links);
